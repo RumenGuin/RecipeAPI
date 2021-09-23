7dd4500de252ecf79e1d63c6098c873694f6f181
Rumen Guin <rumen2k@gmail.com>
2021-09-23 14:42:32 +0530

    Initial Commit

---
+++ RecipeAPI/RecipeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAPIApp.swift
+//  RecipeAPI
+//
+//  Created by RUMEN GUIN on 23/09/21.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
